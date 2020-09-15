a67b5dd7ae4d9d6bcabc292102293624cca0a6a8
Angelrojasdelacruz <Angelrojasdelacruz@gmail>
2020-09-14 22:52:56 -0400

    Edit Owner

--- MyLeasing.Web/Helpers/IUserHelper.cs
+++ MyLeasing.Web/Helpers/IUserHelper.cs
@@ -21,6 +21,11 @@ namespace MyLeasing.Web.Helpers
 
         Task LogoutAsync();
 
+        Task<bool> DeleteUserAsync(string email);
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+
 
     }
 }
